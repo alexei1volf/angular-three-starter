ce93e9563551348722b0ef77e4f8fa26afb155a5
volfav <a.volf@enbisys.com>
2020-04-24 11:22:31 +0700

    some textures

---
+++ src/app/colorGUIHelper.ts
@@ -0,0 +1,15 @@
+export class ColorGUIHelper {
+    private object: any;
+    private prop: any;
+
+    constructor(object, prop) {
+        this.object = object;
+        this.prop = prop;
+    }
+    get value() {
+        return `#${this.object[this.prop].getHexString()}`;
+    }
+    set value(hexString) {
+        this.object[this.prop].set(hexString);
+    }
+}
--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -2,18 +2,22 @@ import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from
 import {
     AmbientLight,
     BoxGeometry,
-    Geometry,
+    DirectionalLight,
+    HemisphereLight,
     Mesh,
     MeshStandardMaterial,
     PCFSoftShadowMap,
     PerspectiveCamera,
     Scene,
     SpotLight,
+    TextureLoader,
     WebGLRenderer
 } from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import {fromEvent, Subscription} from 'rxjs';
 import {SettingsService} from '../settings/settings.service';
+import {ColorGUIHelper} from '../colorGUIHelper';
+import {GUI} from 'dat.gui';
 
 @Component({
     selector: 'app-scene',
@@ -32,9 +36,6 @@ export class SceneComponent implements OnInit, OnDestroy, AfterViewInit {
     private camera: PerspectiveCamera;
     private controls: OrbitControls;
     private subs: Subscription[] = [];
-    private geo: Geometry;
-    private mat: MeshStandardMaterial;
-    private mesh: Mesh;
 
     private get canvas(): HTMLCanvasElement {
         return this.canvasRef.nativeElement;
@@ -44,15 +45,10 @@ export class SceneComponent implements OnInit, OnDestroy, AfterViewInit {
     }
 
     ngOnInit(): void {
-        this.geo = new BoxGeometry(10, 5, 5);
-        this.mat = new MeshStandardMaterial({
-            color: 0xdddddd
-        });
-        this.mesh = new Mesh(this.geo, this.mat);
         this.subs.push(
             fromEvent(window, 'resize').subscribe(event => this.onResize(event)),
-            this.settingsService.roughnessChanged.subscribe(roughness => this.mat.roughness = roughness),
-            this.settingsService.metalnessChanged.subscribe(metalness => this.mat.metalness = metalness)
+            // this.settingsService.roughnessChanged.subscribe(roughness => this.mat.roughness = roughness),
+            // this.settingsService.metalnessChanged.subscribe(metalness => this.mat.metalness = metalness)
         );
     }
 
@@ -62,11 +58,33 @@ export class SceneComponent implements OnInit, OnDestroy, AfterViewInit {
 
     ngAfterViewInit() {
         this.scene = new Scene();
+        const gui = new GUI();
 
-        const ambientLight = new AmbientLight(0xa0a0a0, 0.5);
+        const ambientLight = new AmbientLight(0xffffff, 0.1);
         this.scene.add(ambientLight);
 
-        const spotLight = new SpotLight(0xffffff, 0.5);
+        gui.addColor(new ColorGUIHelper(ambientLight, 'color'), 'value').name('color');
+        gui.add(ambientLight, 'intensity', 0, 1);
+
+        const hemiLight = new HemisphereLight( 0xffffff, 0xffffff, 0.1 );
+        hemiLight.color.setHSL( 0.6, 1, 0.6 );
+        hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
+        hemiLight.position.set( 0, 100, 0 );
+        this.scene.add(hemiLight);
+
+        gui.addColor(new ColorGUIHelper(hemiLight, 'color'), 'value').name('color');
+        gui.add(hemiLight, 'intensity', 0, 1);
+
+        const dirLight = new DirectionalLight( 0xffffff, 0.1 );
+        dirLight.color.setHSL( 0.1, 1, 0.95 );
+        dirLight.position.set( - 1, 2, 1 );
+        dirLight.position.multiplyScalar( 30 );
+        this.scene.add(dirLight);
+
+        gui.addColor(new ColorGUIHelper(dirLight, 'color'), 'value').name('color');
+        gui.add(dirLight, 'intensity', 0, 1);
+
+        const spotLight = new SpotLight(0xffffff, 1);
         spotLight.position.set(-50, 50, 50);
         spotLight.castShadow = true;
         spotLight.shadow.mapSize.width = 1024;
@@ -75,6 +93,9 @@ export class SceneComponent implements OnInit, OnDestroy, AfterViewInit {
         spotLight.shadow.camera.far = 4000;
         spotLight.shadow.camera.fov = 30;
         this.scene.add(spotLight);
+
+        gui.addColor(new ColorGUIHelper(spotLight, 'color'), 'value').name('color');
+        gui.add(spotLight, 'intensity', 0, 2);
 
         const aspectRatio = this.getAspectRatio();
         this.camera = new PerspectiveCamera(this.fieldOfView, aspectRatio, this.nearClippingPane, this.farClippingPane);
@@ -91,14 +112,60 @@ export class SceneComponent implements OnInit, OnDestroy, AfterViewInit {
         this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = PCFSoftShadowMap;
-        this.renderer.setClearColor(0x3a3030, 1);
+        this.renderer.setClearColor(0xffffff, 1);
         this.renderer.autoClear = true;
 
         this.controls = new OrbitControls(this.camera, this.canvas);
         this.controls.rotateSpeed = 1.0;
         this.controls.zoomSpeed = 1.2;
 
-        this.scene.add(this.mesh);
+        const loader = new TextureLoader();
+        const geo = new BoxGeometry(10, 10, 10);
+
+        const mat0 = new MeshStandardMaterial({
+            color: 0xdddddd,
+            roughness: 0.5,
+            metalness: 0.5,
+        });
+        const mesh0 = new Mesh(geo, mat0);
+        mesh0.castShadow = true;
+        mesh0.receiveShadow = true;
+        mesh0.position.set(-20, 0, 0);
+        this.scene.add(mesh0);
+
+        gui.addColor(new ColorGUIHelper(mat0, 'color'), 'value').name('color');
+        gui.add(mat0, 'roughness', 0, 1);
+        gui.add(mat0, 'metalness', 0, 1);
+
+        const mat1 = new MeshStandardMaterial({
+            map: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Color.jpg')
+        });
+        const mesh1 = new Mesh(geo, mat1);
+        mesh1.castShadow = true;
+        mesh1.receiveShadow = true;
+        mesh1.position.set(-10, 0, 0);
+        this.scene.add(mesh1);
+
+        const mat2 = new MeshStandardMaterial({
+            map: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Color.jpg'),
+            normalMap: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Normal.jpg')
+        });
+        const mesh2 = new Mesh(geo, mat2);
+        mesh2.castShadow = true;
+        mesh2.receiveShadow = true;
+        mesh2.position.set(0, 0, 0);
+        this.scene.add(mesh2);
+
+        const mat3 = new MeshStandardMaterial({
+            map: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Color.jpg'),
+            normalMap: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Normal.jpg'),
+            roughnessMap: loader.load('assets/texture/Rock025_2K-JPG/Rock025_2K_Roughness.jpg'),
+        });
+        const mesh3 = new Mesh(geo, mat3);
+        mesh3.castShadow = true;
+        mesh3.receiveShadow = true;
+        mesh3.position.set(10, 0, 0);
+        this.scene.add(mesh3);
 
         this.startRendering();
     }
